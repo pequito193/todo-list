2f7465443981c24d3ddbad58b1f7bdd0a69c9738
pequito193 <vitorspequito@gmail.com>
2022-08-13 13:11:46 +0100

    Added minimum character limit to task name

--- src/newTask.js
+++ src/newTask.js
@@ -4,34 +4,38 @@ function createNewTask() {
     const newTaskProject = document.querySelector('#project-name')
     const taskList = document.querySelector('.task-table')
 
-    const newTask = document.createElement('tr')
-        const taskName = document.createElement('td')
-            taskName.textContent = newTaskName.value
+    if (newTaskName.value.length > 0) {
+        const newTask = document.createElement('tr')
+            const taskName = document.createElement('td')
+                taskName.textContent = newTaskName.value
 
-        const dueDate = document.createElement('td')
-            const dateQuery = document.createElement('input')
-            dateQuery.classList.add('date-query')
-            dateQuery.setAttribute('type', 'date')
+            const dueDate = document.createElement('td')
+                const dateQuery = document.createElement('input')
+                dateQuery.classList.add('date-query')
+                dateQuery.setAttribute('type', 'date')
 
-        const projectName = document.createElement('td')
-            projectName.textContent = newTaskProject.value
-           
+            const projectName = document.createElement('td')
+                projectName.textContent = newTaskProject.value
+            
 
-        dueDate.append(dateQuery)
+            dueDate.append(dateQuery)
 
 
-    newTask.append(taskName, dueDate, projectName)
-    taskList.append(newTask)
+        newTask.append(taskName, dueDate, projectName)
+        taskList.append(newTask)
 
-    const popup = document.querySelector('.new-task-popup')
-        popup.style.visibility = 'hidden'
-        popup.classList.remove('show')
+        const popup = document.querySelector('.new-task-popup')
+            popup.style.visibility = 'hidden'
+            popup.classList.remove('show')
 
-    const body = document.querySelector('.dark')
-        body.style.visibility = 'hidden'
-        body.classList.remove('opaque')
+        const body = document.querySelector('.dark')
+            body.style.visibility = 'hidden'
+            body.classList.remove('opaque')
 
-    newTaskName.value = ''
+        newTaskName.value = ''
+    } else {
+        alert('TASK NAME CANNOT BE SHORTER THAN 1 CHARACTER')
+    }
 }
 
 export {createNewTask};
